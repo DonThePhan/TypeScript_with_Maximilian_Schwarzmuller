f5793d68e4242ebf5e793d2acd4cf6dae9ddd81c
donthephan <donthephan@gmail.com>
2022-10-04 20:56:13 -0400

    2-20 (Tuples) complete

--- 2. TypeScript Basics & Basic Types/1. basics-01-ts-benefits/7. Tuple.ts
+++ 2. TypeScript Basics & Basic Types/1. basics-01-ts-benefits/7. Tuple.ts
@@ -9,20 +9,10 @@ const person: {
   hobbies: ['dancing', 'singing'],
   role: [2,'author']
 }
-
-// person.role.push('admin')
-// person.role[1] = 10
+ 
+/**  */
 
 person.role = [0, 'admin', /*'user'*/] /** will only allow exactly what is specified. No more (i.e. 'user' in this case), no less */
-    
-let favouriteActivities: string[]
-favouriteActivities = ['dancing', 'singing']
-
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase() /** no issue here with toUpperCase because TS knows it's of type string */)
-  console.log(hobby.map() /** here TS flags this because it knows it's working w/ a string so map should've be available */)
-}
 
 //! makes it clear this file is an ES module -> No redeclare errors
 export {};
---
+++ 2. TypeScript Basics & Basic Types/1. basics-01-ts-benefits/8. Enum.ts
@@ -0,0 +1,33 @@
+/** ENUM */
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+
+/** the enum above creates this below - var declaration, followed by an anonymous function 
+ * that immediately executes w/ parameters 'Role || (Role = {})' */
+/* 
+* var Role;
+* (function (Role) {
+*    Role[Role["ADMIN"] = 0] = "ADMIN";
+*    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+*    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+* })(Role || (Role = {}));
+*/
+
+/** which makes an object like this: */
+/* 
+* {0: 'ADMIN', 1: 'READ_ONLY', 2: 'AUTHOR', ADMIN: 0, READ_ONLY: 1, AUTHOR: 2}
+ */
+
+
+const person = {
+  name: 'Donny', 
+  age: 36,       
+  hobbies: ['dancing', 'singing'],
+  role: Role.ADMIN
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author')
+}
+
+//! makes it clear this file is an ES module -> No redeclare errors
+export {};
