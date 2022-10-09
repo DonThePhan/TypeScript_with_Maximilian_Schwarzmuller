92fcf874068935d2548d4535afef02a96bdc495d
donthephan <donthephan@gmail.com>
2022-10-08 20:41:31 -0400

    2-70 (Singleton Patter) complete

--- 5. Classes & Interfaces/src/6. Singleton Pattern.ts
+++ 5. Classes & Interfaces/src/6. Singleton Pattern.ts
@@ -104,3 +104,5 @@ accounting.addEmployee('Donny');
 accounting.addEmployee('Lex');
 
 accounting.describe();
+
+export {}; //! Remove this line when compiling. Purpose is to not interfere w/ other TS files that have the same variables.
