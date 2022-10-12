af9ee29a3d657cbb226fd4d4d2a637413445ebed
donthephan <donthephan@gmail.com>
2022-10-11 23:43:48 -0400

    6-83 (Intersection Types) complete

---
+++ 6. Advanced Types/src/app.ts
@@ -0,0 +1,30 @@
+/** INTERSECTION TYPES*/
+
+// interface Admin { // INTERFACE Alternative
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+// interface Employee { // INTERFACE Alternative
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+/** use "&" for INTERSECTION */
+// interface ElevatedEmployee extends Admin, Employee {}  // INTERFACE Alternative
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Donny',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+/** Below we have 2 UNION types */
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+/** w/ UNION types, INTERSECTION acts like a VENN Diagram. In this case the only type that works is 'number' */
+type Universal = Combinable & Numeric;
