6ca3f4ce13f556a97be423d31270e2ec9b29bfa1
donthephan <donthephan@gmail.com>
2022-10-16 15:47:06 -0400

    7-94 (Built in Generic Types) complete

---
+++ 7. Generics/src/1. Built in Generic Types.ts
@@ -0,0 +1,25 @@
+/** GENERIC Types - A type that works w/ other types - Array, Promise */
+
+//* Array - specify what data type is in the Array
+const names: Array<string> = ['Donny', 'Lana']; // TSC alternative to "string[]"
+console.log(names);
+
+//* Promises - Specify what data we will return
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+
+  reject('error');
+});
+
+promise
+  .then((data) => {
+    // data.split(' '); TSC will flag because it knows we're dealing w/ numbers
+    console.log(data);
+  })
+  .catch((data) => {
+    console.log(data);
+  });
+
+export {}; //! Remove this line when compiling. Purpose is to not interfere w/ other TS files that have the same variables.
