d9adc7e2bdf0b31ec79570f465ab29d27d6645b5
donthephan <donthephan@gmail.com>
2022-10-18 22:46:45 -0400

    7-97 (Generic Types) complete

--- 7. Generics/src/1. Built in Generic Types.ts
+++ 7. Generics/src/1. Built in Generic Types.ts
@@ -1,7 +1,7 @@
 /** GENERIC Types - A type that works w/ other types - Array, Promise */
 
 //* Array - specify what data type is in the Array
-const names: Array<string> = ['Donny', 'Lana']; // TSC alternative to "string[]"
+const names: Array<string> = ['Donny', 'Lana']; // TS alternative to "string[]"
 console.log(names);
 
 //* Promises - Specify what data we will return
@@ -15,7 +15,7 @@ const promise: Promise<number> = new Promise((resolve, reject) => {
 
 promise
   .then((data) => {
-    // data.split(' '); TSC will flag because it knows we're dealing w/ numbers
+    // data.split(' '); //! TSC will flag because it knows we're dealing w/ numbers
     console.log(data);
   })
   .catch((data) => {
--- 7. Generics/src/app.ts
+++ 7. Generics/src/app.ts
@@ -1,23 +1,35 @@
-/** GENERIC Types - A type that works w/ other types - Array, Promise */
+/** CREATING GENERIC FUNCTIONS - used to infer types without specifying - keeps types flexible */
 
-//* Array - specify what data type is in the Array
-const names: Array<string> = ['Donny', 'Lana']; // TSC alternative to "string[]"
-console.log(names);
+//* in the func below we declared 2 types and gave them aliases (T & U) - this method is good for intersection when the 2 types are likely to be different
+function merge<T extends object /** <- constraints */, U, V>(
+  objA: T,
+  objB: U,
+  objC: V,
+) {
+  return Object.assign(objA, objB, objC); // JS merger function.  We could have used another method. This is just and example.
+}
+const mergedObj = merge({ name: 'Donny' }, { age: 36 }, { employed: false }); //* we didn't need to specify name:string & age:number
 
-//* Promises - Specify what data we will return
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
+console.log(mergedObj.employed);
 
-  reject('error');
-});
+//* Example 2
+interface Lengthy {
+  length: number;
+}
 
-promise
-  .then((data) => {
-    // data.split(' '); TSC will flag because it knows we're dealing w/ numbers
-    console.log(data);
-  })
-  .catch((data) => {
-    console.log(data);
-  });
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+
+  if (element.length === 1) {
+    descriptionText = `Got 1 element`;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['hello world', 'cooking']));
+console.log(countAndDescribe('hello world'));
+// console.log(countAndDescribe(12)); // TS would flag because, for this case, we are looking for a "length" property that doesn't exist
+
